581e8043c18d9ce6f2fb96b78f7ae633a6615546
José Rodrigo <jose.rs.inacio@gmail.com>
2021-10-24 02:07:13 -0300

    Criados Serviços de acesso ao banco de dados

---
+++ service/src/global/usuario.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import {
+  Usuario,
+  Prisma
+} from '@prisma/client';
+
+
+@Injectable()
+export class UsuarioService {
+  constructor(private prisma: PrismaService) {}
+
+  async usuario(usuarioWhereUniqueInput: Prisma.UsuarioWhereUniqueInput): Promise<Usuario | null> {
+    return this.prisma.usuario.findUnique({
+      where: usuarioWhereUniqueInput,
+    });
+  }
+
+  async usuarios(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UsuarioWhereUniqueInput;
+    where?: Prisma.UsuarioWhereInput;
+    orderBy?: Prisma.UsuarioOrderByWithRelationInput;
+  }): Promise<Usuario[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+
+    return this.prisma.usuario.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createUser(data: Prisma.UsuarioCreateInput): Promise<Usuario> {
+    return this.prisma.usuario.create({
+      data,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UsuarioWhereUniqueInput;
+    data: Prisma.UsuarioUpdateInput;
+  }): Promise<Usuario> {
+    const { where, data } = params;
+
+    return this.prisma.usuario.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UsuarioWhereUniqueInput): Promise<Usuario> {
+    return this.prisma.usuario.delete({
+      where,
+    });
+  }
+}
